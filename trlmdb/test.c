288a76b8de0cee7c1531ac2735b376f3aa62104e
Morten Krogh <morten.krogh@amberbio.com>
2016-02-05 15:36:36 +0100

    testing

--- trlmdb/test.c
+++ trlmdb/test.c
@@ -40,6 +40,34 @@ void test_message()
 	
 }
 
+void populate_db_1 (void)
+{
+	int rc = 0;
+	TRLMDB_env *env;
+
+	rc = trlmdb_env_create(&env);
+	if (rc) print_error(rc); 
+	
+	rc = trlmdb_env_open(env, "./trlmdb-1", 0, 0644);
+	if (rc) print_error(rc);
+
+	TRLMDB_txn *txn;
+	
+	rc = trlmdb_txn_begin(env, NULL, 0, &txn);
+	if (rc) print_error(rc);
+
+	MDB_val key_1 = {1, "key_1"};
+	MDB_val val_1 = {2, "val_1"};
+	
+	rc = trlmdb_put(txn, &key_1, &val_1);
+	if (rc) print_error(rc);
+
+	rc = trlmdb_txn_commit(txn);
+	if (rc) print_error(rc);
+}
+
+
+
 void trlmdb_test (void)
 {
 	int rc = 0;
